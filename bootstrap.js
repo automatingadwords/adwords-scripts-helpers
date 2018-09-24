83f0d5e5b99bd0694fd7b4b28f1b01dc507c98d2
Automating AdWords <charles@automatingadwords.com>
2018-09-24 12:13:19 -0400

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,8 @@
-eval("https://raw.githubusercontent.com/automatingadwords/adwords-scripts-helpers/master/helpers.js");
-eval("https://raw.githubusercontent.com/automatingadwords/adwords-scripts-helpers/master/settings.js");
+var url = "https://raw.githubusercontent.com/automatingadwords/adwords-scripts-helpers/master/helpers.js";
+var content = UrlFetchApp.fetch(url).getContentText();
+eval(content);
+
+var url = "https://raw.githubusercontent.com/automatingadwords/adwords-scripts-helpers/master/settings.js";
+var content = UrlFetchApp.fetch(url).getContentText();
+eval(content);
+
