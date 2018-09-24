6b22425bdb05466f27a12eb7be3ccac577aee54b
Automating AdWords <charles@automatingadwords.com>
2018-09-24 13:50:42 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,122 @@
 
 function Setting () {
 
-  this.init = function(){
-    log("Settings pulled in...");
+  this.scanForAccounts = function(HEADER_TYPES) {
+   var map = {};
+   var controlSheet = SpreadsheetApp.openByUrl(INPUT_SHEET_URL).getSheetByName(INPUT_TAB_NAME)
+   var data = SpreadsheetApp.openByUrl(INPUT_SHEET_URL).getSheetByName(INPUT_TAB_NAME).getDataRange().getValues();
+   data.shift();
+   data.shift();
+   data.shift();
+    
+   var HEADER = Object.keys(HEADER_TYPES)
+ 
+   var LOGS_COLUMN = 0;
+   var col = 5
+   while(controlSheet.getRange(3, col).getValue()){
+     LOGS_COLUMN = controlSheet.getRange(3, col).getValue() == "Logs" ? col : 0;
+     if(LOGS_COLUMN>0){break;}
+     col++;
+   }
+ 
+   for(var k in data) {
+     //if "run script" is not set to "yes", continue.
+     if(data[k][0] == '' || data[k][3].toLowerCase() != 'yes') { continue; }
+     var rowNum = parseInt(k,10) + 4;
+     var id = data[k][0];
+     var rowId = id+"/"+rowNum;
+     map[id] = map[id]  || {}
+     map[id][rowId] = { 'ROW_NUM': (parseInt(k,10) + 4) };
+     for(var j in HEADER) {
+       if(HEADER[j] == "LOGS_COLUMN"){
+         map[id][rowId][HEADER[j]] = LOGS_COLUMN;
+         continue;
+       }
+       map[id][rowId][HEADER[j]] = data[k][j];
+     }    
+   }
+ 
+   for(var id in map){
+     for(var rowId in map[id]){
+       for(var key in map[id][rowId]){
+         map[id][rowId][key] = processSetting(key,map[id][rowId][key], HEADER_TYPES, controlSheet)
+       }
+     }
+   }
+   return map;
+ }
+ 
+
+ this.parseDateRange = function(SETTINGS) {
+    var YESTERDAY = getAdWordsFormattedDate(1, 'yyyyMMdd');
+    SETTINGS.DATE_RANGE = '20000101,' + YESTERDAY;
+  
+    if(SETTINGS.DATE_RANGE_LITERAL == 'LAST_N_DAYS') {
+      SETTINGS.DATE_RANGE =  getAdWordsFormattedDate(SETTINGS.N, 'yyyyMMdd') + ',' + YESTERDAY;
+    }
+  
+    if(SETTINGS.DATE_RANGE_LITERAL == 'LAST_N_MONTHS') {
+      var now = new Date(Utilities.formatDate(new Date(), AdWordsApp.currentAccount().getTimeZone(), 'MMM dd, yyyy HH:mm:ss'));
+      now.setHours(12);
+      now.setDate(0);
+  
+      var TO = Utilities.formatDate(now, 'PST', 'yyyyMMdd');
+      now.setDate(1);
+  
+      var counter = 1;
+      while(counter < SETTINGS.N) {
+        now.setMonth(now.getMonth()-1);
+        counter++;
+      }
+  
+      var FROM = Utilities.formatDate(now, 'PST', 'yyyyMMdd');
+      SETTINGS.DATE_RANGE =  FROM + ',' + TO;
+    }
+  
   }
+ 
+ this.processSetting = function (key, value, HEADER,controlSheet){
+   var type = HEADER[key]
+   if(key=="ROW_NUM"){
+     return value;
+   }
+   var h = new Helper();
+   switch(type){
+     case "number":
+        if(h.isNumber(value)){
+          return value
+        }else{
+          throw("Error: Expected a number but recieved " +value+". Please check the settings")
+        }
+        return value
+        break;
+     case "label":
+       return [controlSheet.getRange(3,Object.keys(HEADER).indexOf(key)+1).getValue(),value]
+       break;
+     case "normal":
+       return value
+       break;
+     case "bool":
+       return value == "Yes" ? true : false;
+       break;
+     case "csv":
+       var ret =  value.split(",")
+       ret = ret[0]==""&&ret.length==1?[]:ret;
+       if(ret.length==0){
+         return [];
+       }else{
+         for(var r in ret){
+           ret[r] = String(ret[r]).trim() 
+         }
+       }
+       return ret;
+       break;
+     default:
+       throw("error setting type "+type+" not recognised for "+key)
+ 
+   }
+ }
+
+
   
 }
